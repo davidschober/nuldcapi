346c6b3a6405cc6544d22add97789e7ab51417da
davidschober <david.schober@northwestern.edu>
2019-12-04 14:16:37 -0600

    moved script to a separate folder to make it more packageable

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+# This is the skeleton replace "NAME" with the name of the project
+setup(name='nuproxy helpers',
+        version='0.1',
+        description='put some text here',
+        url='put the url of git',
+        author='David Schober',
+        author_email='davidschob@gmail.com',
+        license='MIT',
+        packages=['nuproxy'],
+        #Require packages if you need them
+        install_requires=[
+            '',
+            ],
+        #These are used instead of bin packages
+        entry_points = {
+            'console_scripts': ['collection_csv=nuproxy.save_collection_as_csv:main'],
+            },
+        include_package_data=True,
+        zip_safe=False)
