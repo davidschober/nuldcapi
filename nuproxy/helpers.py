2ef3b839023ffc596f41fec25f89bff6b02ab83a
davidschober <david.schober@northwestern.edu>
2020-01-10 16:05:33 -0600

    Fix String bug, add --allfields flag (#7)
    * refactor so that I can have an "all-fields" flag TODO: add all fields to commandine.py
    
    * added -a flag for all fields
    
    * fixed small sort bug
    
    * flattened all titles into a single string for --allfields. I think this works better than having the hash."
    
    * made environment optional with a default to production
    
    * added basic tests
    
    * added basic tests

--- nuproxy/helpers.py
+++ nuproxy/helpers.py
@@ -57,7 +57,7 @@ def flatten_metadata(source_dict, field):
 
     ## Example
 
-    >>> source = {'title':{'primary':['title1','title2']}, 
+    >>> source = {'title':{'primary':['title1','title2'],'alternate':['alt1','alt2']}, 
     ...          'thumbnail_url':'http://thumb', 
     ...          'list_field':['1','2','3'],
     ...          'list_of_dicts':[{'label':'label1','uri':'uri1'}, {'label':'label2','uri':'uri2'}],
@@ -66,8 +66,10 @@ def flatten_metadata(source_dict, field):
     
 
     >>> flatten_metadata(source, 'title')
+    'title1 | title2 | alt1 | alt2'
+    
+    >>> flatten_metadata(source, 'primary-title')
     'title1 | title2'
-
 
     >>> flatten_metadata(source, 'dict_field')
     'dict_label'
@@ -89,23 +91,34 @@ def flatten_metadata(source_dict, field):
     field_data = source_dict.get(field)
 
     if field == 'title':
+        #join a bunch of title together, regardless of primary or alternate
+        # Note, this could work to join any multi-dimensional field hard, but I'm not 
+        # That makes sense. 
+        all_titles = [title for title_lists in field_data.values() for title in title_lists]
+        field_metadata = ' | '.join(all_titles)
+
+    elif field == 'primary-title':
+        field = 'title'
         # Titles are special, if you request just 'title' get primary.
         # there may be more than one title, join it by a semicolon
+        
+        field_data = source_dict.get(field)
         field_metadata = ' | '.join(field_data.get('primary'))
 
     elif field == 'alternate-title':
         # grab the alternate title. Hardcoded this one. It's a special case
         field = 'title'
-        field_data = work.get('_source').get(field) 
+
+        field_data = source_dict.get(field)
         field_metadata = ' | '.join(field_data.get('alternate'))
 
     elif field == 'permalink':
         # prepend the resolver url to the front of the ark
-        field_metadata = 'https://n2t.net/'+field_data
+        field_metadata = f"https://n2t.net/{field_data}"
 
     elif field == 'thumbnail_url':
         # This just makes resolve to a jpg. I should make this configurable at the commandline
-        field_metadata = field_data+'/full/!300,300/0/default.jpg'
+        field_metadata = f"{field_data}/full/!300,300/0/default.jpg"
 
     elif type(field_data) is dict:
         # print(field_data)
@@ -116,7 +129,7 @@ def flatten_metadata(source_dict, field):
         # no label. Join it by a semi-colon. Take into account that some items are dicts with labels
         # others are just a list of strings. I haven't found anything else. 
 
-        field_metadata = ' | '.join([item.get('label', item) if type(item) is dict else item for item in field_data])
+        field_metadata = ' | '.join([str(item.get('label', item)) if type(item) is dict else str(item) for item in field_data])
 
     else:
         # These should be straight strings. 
@@ -128,13 +141,20 @@ def get_results_as_list(search_results, fields):
     """ Gets all items in a collection and returns the identified fields(list)
     This function flattens all nested data ham-fistedly, favoring labels over URIs for
     all metadata. Any list elements are separated by a semi-colon and turned to a string. 
-
     """
 
     for work in search_results:
         #Get the metadata dictionary
         work_metadata = work.get('_source')
         yield [flatten_metadata(work_metadata, field) for field in fields]
+
+def get_all_fields_from_set(search_results):
+    """ returns a flat, unique list of all fields from a search query. This can be fed back
+    into a fresh query result to flatten the results for a CSV. It is not as efficient as 
+    passing fields directly as you have to make two queries
+    """
+    
+    return list(set([field for work in search_results for field in work.get('_source').keys()]))
 
 def save_as_csv(headers, data, output_file):
     """outputs a CSV using unicodecsv"""
