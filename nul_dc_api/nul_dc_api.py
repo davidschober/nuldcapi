af7a21cf67ddb21765d5c0bbd02d63e4eca2e03b
David Schober <davidschob@gmail.com>
2020-11-03 14:50:16 -0600

    put functions in alpha

--- nul_dc_api/nul_dc_api.py
+++ nul_dc_api/nul_dc_api.py
@@ -2,23 +2,23 @@ import elasticsearch
 from elasticsearch import helpers
 import unicodecsv as csv
 
-def get_search_results(environment, query):
-    """Takes an environment and a query and returns an iterable of all results
-    using the 'scan' function in es.helpers. Scan is an efficient pager.
+def filter_works_by_fileset_matching(work_results, fileset_id_list):
+    """Matches a fileset name against match. This function is used to grab all filesets 
+    matching a wildcard if the results have said fileset, then it will return a generator 
+    with those works
+
+    ## Example
+    >>> works = [{'_source':{'member_ids':['1','2','3']}},
+    ...     {'_source':{'member_ids':['5','6','7']}},
+    ...     {'_source':{'member_ids':['2','3','4']}}]
+    >>> fids = ['1','2']
+    >>> list(filter_works_by_fileset_matching(works,fids))
+    [{'_source': {'member_ids': ['1', '2', '3']}}, {'_source': {'member_ids': ['2', '3', '4']}}]
     """
 
-    # pick an environment 
-    if environment == 'production':
-        proxy = 'https://dcapi.stack.rdc.library.northwestern.edu/search/'
-        print(proxy)
-    if environment == 'staging':
-        proxy = 'https://dcapi.stack.rdc-staging.library.northwestern.edu/search/'
-    # create an es instance 
-    # added ssl and port 443 to see if it solves timeout issue
-    es = elasticsearch.Elasticsearch(proxy, send_get_body_as='POST', timeout=30, max_retries=10, retry_on_timeout=True)
-    # return the results 
-    # return es.search(index='common', body=query)
-    return helpers.scan(es, query=query, index='common')
+    for work in work_results:
+        if any(fid in fileset_id_list for fid in work.get('_source').get('member_ids')):
+            yield work 
 
 def flatten_metadata(source_dict, field):
     """ Takes a nested dictionary of a work's metadata, gets and flattens a field. 
@@ -82,22 +82,23 @@ def flatten_metadata(source_dict, field):
     
     return ' | '.join(flatten_to_list(field_metadata)) 
 
-def get_results_as_list(search_results, fields):
-    """ Gets all items in a collection and returns the identified fields(list)
-    This function flattens all nested data ham-fistedly, favoring labels over URIs for
-    all metadata. Any list elements are separated by a semi-colon and turned to a string. 
-
-    ## Example
-    >>> res = [{'_source': {'key':'1', 'key2':'2', 'key3':'3'}}, 
-    ...    {'_source':{'key1':'1-2', 'key3':'1-3', 'key2':'1-2'}}]
-    >>> list(get_results_as_list(res, ['key','key3']))
-    [['1', '3'], ['None', '1-3']]
+def get_search_results(environment, query):
+    """Takes an environment and a query and returns an iterable of all results
+    using the 'scan' function in es.helpers. Scan is an efficient pager.
     """
 
-    for work in search_results:
-        #Get the metadata dictionary
-        work_metadata = work.get('_source')
-        yield [flatten_metadata(work_metadata, field) for field in fields]
+    # pick an environment 
+    if environment == 'production':
+        proxy = 'https://dcapi.stack.rdc.library.northwestern.edu/search/'
+        print(proxy)
+    if environment == 'staging':
+        proxy = 'https://dcapi.stack.rdc-staging.library.northwestern.edu/search/'
+    # create an es instance 
+    # added ssl and port 443 to see if it solves timeout issue
+    es = elasticsearch.Elasticsearch(proxy, send_get_body_as='POST', timeout=30, max_retries=10, retry_on_timeout=True)
+    # return the results 
+    # return es.search(index='common', body=query)
+    return helpers.scan(es, query=query, index='common')
 
 def get_all_fields_from_set(search_results):
     """ returns a flat, unique list of all fields from a search query. This can be fed back
@@ -115,13 +116,29 @@ def get_all_fields_from_set(search_results):
 
     return list(set([field for work in search_results for field in work.get('_source').keys()]))
 
-def save_as_csv(headers, data, output_file):
-    """outputs a CSV using unicodecsv"""
-    with open(output_file, 'wb') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(headers)
-        for row in data:
-            writer.writerow(row)
+def get_fileset_ids_with_title_matching(environment, match):
+    """ Returns a list of ids with titles matching a wildcard. e.g. '*.tif' to
+    find all filesets that still have a title with 'tif' in it."""
+    
+    file_results = get_search_results(environment, query_for_query_string('FileSet', match))
+    return [f.get('_id') for f in file_results]
+
+def get_results_as_list(search_results, fields):
+    """ Gets all items in a collection and returns the identified fields(list)
+    This function flattens all nested data ham-fistedly, favoring labels over URIs for
+    all metadata. Any list elements are separated by a semi-colon and turned to a string. 
+
+    ## Example
+    >>> res = [{'_source': {'key':'1', 'key2':'2', 'key3':'3'}}, 
+    ...    {'_source':{'key1':'1-2', 'key3':'1-3', 'key2':'1-2'}}]
+    >>> list(get_results_as_list(res, ['key','key3']))
+    [['1', '3'], ['None', '1-3']]
+    """
+
+    for work in search_results:
+        #Get the metadata dictionary
+        work_metadata = work.get('_source')
+        yield [flatten_metadata(work_metadata, field) for field in fields]
 
 def query_for_query_string(model, match):
     """ Uses teh query string query to return results. Examples on the elasticsearch
@@ -180,31 +197,6 @@ def query_works_with_multiple_filesets():
             }
     return query
 
-def get_fileset_ids_with_title_matching(environment, match):
-    """ Returns a list of ids with titles matching a wildcard. e.g. '*.tif' to
-    find all filesets that still have a title with 'tif' in it."""
-    
-    file_results = get_search_results(environment, query_for_query_string('FileSet', match))
-    return [f.get('_id') for f in file_results]
-
-def filter_works_by_fileset_matching(work_results, fileset_id_list):
-    """Matches a fileset name against match. This function is used to grab all filesets 
-    matching a wildcard if the results have said fileset, then it will return a generator 
-    with those works
-
-    ## Example
-    >>> works = [{'_source':{'member_ids':['1','2','3']}},
-    ...     {'_source':{'member_ids':['5','6','7']}},
-    ...     {'_source':{'member_ids':['2','3','4']}}]
-    >>> fids = ['1','2']
-    >>> list(filter_works_by_fileset_matching(works,fids))
-    [{'_source': {'member_ids': ['1', '2', '3']}}, {'_source': {'member_ids': ['2', '3', '4']}}]
-    """
-
-    for work in work_results:
-        if any(fid in fileset_id_list for fid in work.get('_source').get('member_ids')):
-            yield work 
-
 def results_to_simple_dict(results, fields, fieldmap=None):
     """Takes a list of formatted results and a set of fields and maps to a simple dict.
     This can be passed to something like dicttoxml to generate xml. 
@@ -222,6 +214,14 @@ def results_to_simple_dict(results, fields, fieldmap=None):
         fields = fieldmap
     # create a generator so that we can reserve memory on large datasets
     return (dict(zip(fields,work_meta)) for work_meta in results_list)
+
+def save_as_csv(headers, data, output_file):
+    """outputs a CSV using unicodecsv"""
+    with open(output_file, 'wb') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(headers)
+        for row in data:
+            writer.writerow(row)
    
 def save_xml(res_dict, output_file):
     """takes results as a list of dicts and writes them out to xml"""
